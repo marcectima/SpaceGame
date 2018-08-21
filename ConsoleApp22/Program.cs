0a61a5dd78d36147d3a5bec3925d9e9e120875fd
marcectima <timhoward5@yahoo.com>
2018-08-21 14:31:08 -0400

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] username = new string[1];
+            Console.WriteLine("Enter your name");
+            username[0] = Console.ReadLine(1);
+            Console.WriteLine("This " + username[0] + " is awsome");          
+        }
+    }
+}
